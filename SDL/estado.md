f08a765b308dd331c1bf6c8cddda2c1629b2310e
Henrique Bejgel <henrique.bejgel@21212.com>
2015-04-14 00:18:23 -0300

    Dei uma polida no jogo, adicionei reset e vidas (alocadas dinamicamente) e incluid o arquivo explicando os estados

---
+++ SDL/estado.md
@@ -0,0 +1,5 @@
+Variáveis de estado no jogo do basquete:
+
+status - A variável status que controla o estado do jogo (bola caindo, indo para a esquerda, indo para a direita, pountuação, etc...)
+
+dt - A variável dt é responsável por calcular a diferença de tempo entre a última passagem pelo loop e o tempo atual, a posição da bola é atualizada de acordo com essa variável.
--- SDL/joguinho.c
+++ SDL/joguinho.c
@@ -1,25 +1,26 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <assert.h>
-int move_ball(int status, SDL_Rect* r, int min, int max, int speed){
+
+int move_ball(int status, SDL_Rect* r, int min, int max, float dx){
     switch(status)
     {
         case 0:
-            r->x += speed;
+            r->x += dx;
             if(r->x >= max){
                 return 1;
             }
             return 0;
             break;
         case 1:
-            r->x -= speed;
+            r->x -= dx;
             if(r->x <= min){
                 return 0;
             }
             return 1;
             break;
         case 2:
-            r->y += speed;
+            r->y += dx;
             if(r->y > 480){
                 r->y = 20;
                 if(r->x > 50 && r->x < 90)
@@ -51,7 +52,6 @@ int main (int argc, char* args[])
     assert(renderer != NULL);
     SDL_SetRenderDrawColor(renderer, 0xFF,0xFF,0xFF,0x00);
     SDL_RenderFillRect(renderer, NULL);
-    SDL_Rect score = {500, 300, 100, 100};
 
     /* EXECUTION */
     SDL_Surface * ball_img = SDL_LoadBMP("basketball.bmp");
@@ -65,13 +65,16 @@ int main (int argc, char* args[])
         assert(0);
     }
     int status = 0;
-    float gravity = 9.8;
-    int fall_time = 0;
-    float speed = 20;
-    float game_speed = 20;
-    int last_moved = 0;
+    float gravity = 0.15;
+    float falling_speed = 0;
+    float game_speed = 0.5;
+    int dt = 0;
     int green = 0x00;
     int red = 0x00;
+    int vidas = 3;
+    SDL_Rect* ret_vidas = NULL;
+    int acumulador = 0;
+    Uint32 last = 0;
     SDL_Texture * ball_texture = SDL_CreateTextureFromSurface(renderer, ball_img);
     SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
     SDL_Rect dst = {0, 420, 200, 100};
@@ -79,57 +82,84 @@ int main (int argc, char* args[])
     SDL_Event e;
     while (1)
     {
-        int ticks = SDL_GetTicks();
         SDL_WaitEventTimeout(&e, 10);
         if (e.type == SDL_QUIT) {
             break;
         } else if (e.type == SDL_MOUSEBUTTONDOWN){
-            status = 2;
-            fall_time = ticks-1;
+            if(status == 0 || status == 1){
+                status = 2;
+                falling_speed = 0;
+            }
+        } else if (e.type == SDL_KEYDOWN){
+            SDL_Keycode keyPressed = e.key.keysym.sym;
+            if(keyPressed == SDLK_r){
+                vidas = 3;
+                status = 0;
+                ball.x = 20;
+                ball.y = 50;
+            }
         }
 
-        if(ticks - last_moved > 30){
-            if(fall_time == 0){
-                printf("%f\n", game_speed);
-                status = move_ball(status, &ball, 20, 500, game_speed);
+        // update no tempo
+        Uint32 now = SDL_GetTicks();
+        if(now > last){
+            dt = now-last;
+            last = now;
+        }
+
+        // Se a bola está indo para a esquerda ou indo para a direita
+        if(status == 0 || status == 1){
+            // incrementa a sua posição
+            status = move_ball(status, &ball, 20, 500, dt*game_speed);
+        }else if (status == 2){ // bola caindo
+            falling_speed = dt*gravity;
+            status = move_ball(status, &ball, 20, 500, dt*falling_speed);
+        }
+        // acertou a cesta
+        else if(status == 3){
+            acumulador += 1;
+            if(acumulador > 1 && vidas < 3){
+                vidas++;
             }
-            else{
-                speed = ((float)(ticks-fall_time)/100)*gravity;
-                printf("speed:%f\n", speed);
-                status = move_ball(status, &ball, 20, 500, speed);
-                if(status != 2){
-                    speed = 20;
-                    fall_time = 0;
-                }
+            game_speed *= 2;
+            red = 0x00;
+            green = 0xff;
+            status = 0;
+        }
+        // errou a cesta
+        else if(status == 4){
+            if(game_speed > 0.5){
+                game_speed /= 2;
             }
-            if(status == 3)
-            {
-                game_speed *= 2;
-                red = 0x00;
-                green = 0xff;
-                status = 0;
-            }
-            else if(status == 4)
-            {
-                if(game_speed > 20)
-                {
-                    game_speed /= 2;
-                }
-                red = 0xff;
-                green = 0x00;
-                status = 0;
-            }
-            last_moved = ticks;
+            acumulador = 0;
+            vidas -= 1;
+            red = 0xff;
+            green = 0x00;
+            status = 0;
+        }
+        if(vidas == 0){
+            status = 5;
         }
 
 
         SDL_SetRenderDrawColor(renderer, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderFillRect(renderer, NULL);
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
+        if(ret_vidas!=NULL){
+            free(ret_vidas);     
+        }
+        ret_vidas = (SDL_Rect*)malloc(vidas*sizeof(SDL_Rect));
+        for(int i=0; i<vidas; i++){
+            ret_vidas[i].x = 500 + (i*30);
+            ret_vidas[i].y = 50;
+            ret_vidas[i].w = 25;
+            ret_vidas[i].h = 25; 
+            //life = {500+i*30, 50, 25, 25};
+            SDL_RenderFillRect(renderer, &ret_vidas[i]);
+        }
         SDL_RenderCopy(renderer, ball_texture, NULL, &ball);
         SDL_RenderCopy(renderer, texture, NULL, &dst);
         SDL_SetRenderDrawColor(renderer, red, green,0x00,0x00);
-        SDL_RenderFillRect(renderer, &score);
-
         SDL_RenderPresent(renderer);
     }
 
